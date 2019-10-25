0e7d80b68354ec2b57e27958b416408b7d4b1bc4
ma981son <ma981son@htwg-konstanz.de>
2019-10-25 13:22:48 +0200

    Pig scala test

--- src/main/scala/de/htwg/se/cards/model/Deck.scala
+++ src/main/scala/de/htwg/se/cards/model/Deck.scala
@@ -3,10 +3,8 @@ package de.htwg.se.cards.model
 case class Deck() {
   val suits = List("DIAMONDS", "HEARTS", "CLOVER", "SPADES")
   val values = List("7", "8", "9", "10", "JACK", "QUEEN", "KING", "ASS")
-  val cardList: List[Card] = for {
+  val cards: List[Card] = for {
     s <- suits
     v <- values
   } yield Card(s, v)
-
-  def cards(): List[Card] = cardList
 }
--- src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
+++ src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
@@ -4,12 +4,11 @@ import org.scalatest.{Matchers, WordSpec}
 
 class TalonSpec extends WordSpec with Matchers {
   "A Talon" when {
-    val t32 = Talon(32)
-    "new" should {
-      "have a capacity of" in {
-        t32.capacity should be {
-          32
-        }
+    val d = Deck().cards
+    val talon = Talon(d)
+    "filled" should {
+      "should shuffle" in {
+        assert(!talon.equals(talon.shuffle()))
       }
     }
   }
