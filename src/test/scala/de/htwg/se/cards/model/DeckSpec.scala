9b475cc0d0f429b181d72cfe3648193bbea4c7d8
Alex Schapelt <al-schapelt@t-online.de>
2019-10-25 13:22:10 +0200

    made CardSpec and DeckSpec

--- src/test/scala/de/htwg/se/cards/model/DeckSpec.scala
+++ src/test/scala/de/htwg/se/cards/model/DeckSpec.scala
@@ -3,5 +3,16 @@ package de.htwg.se.cards.model
 import org.scalatest.{Matchers, WordSpec}
 
 class DeckSpec extends WordSpec with Matchers {
-
+  "A Deck" should {
+    val d = Deck()
+    "have a Card of a 32 Deck" in {
+      d.cards.contains(Card("DIAMONDS", "7")) should be(true)
+    }
+    "have 32 Cards" in {
+      d.cards.size should be(32)
+    }
+    "not have other cards" in {
+      d.cards.contains(Card("sdhaksdh", "lksajd")) should be(false)
+    }
+  }
 }
--- src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
+++ src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
@@ -3,14 +3,4 @@ package de.htwg.se.cards.model
 import org.scalatest.{Matchers, WordSpec}
 
 class TalonSpec extends WordSpec with Matchers {
-  "A Talon" when {
-    val t32 = Talon(32)
-    "new" should {
-      "have a capacity of" in {
-        t32.capacity should be {
-          32
-        }
-      }
-    }
-  }
 }
