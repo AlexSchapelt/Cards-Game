11f84c39eabf9e9d3dc2eedfb83c8fd5e16c3b1e
Alex Schapelt <al-schapelt@t-online.de>
2019-10-25 09:31:00 +0200

    edited Cards

--- src/main/scala/de/htwg/se/cards/Cards.scala
+++ src/main/scala/de/htwg/se/cards/Cards.scala
@@ -6,5 +6,7 @@ object Cards {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
+    val divide = Rational(1, 2).divide(Rational(2, 3)).reduce()
+    println(divide)
   }
 }
