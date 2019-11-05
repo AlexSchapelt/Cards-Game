7bfd9c982a200776a55b2fca03bdb0751642fe3c
Alex Schapelt <al-schapelt@t-online.de>
2019-11-05 21:16:08 +0100

    added drop method to talon

--- src/main/scala/de/htwg/se/cards/model/Talon.scala
+++ src/main/scala/de/htwg/se/cards/model/Talon.scala
@@ -2,6 +2,10 @@ package de.htwg.se.cards.model
 import scala.util.Random
 
 case class Talon (cards: List[Card]) {
+  def drop(): (Talon, Card) = {
+    (Talon(cards.tail), cards.head)
+  }
+
   def shuffle(): Talon = {
     Talon(Random.shuffle(this.cards))
   }
--- src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
+++ src/test/scala/de/htwg/se/cards/model/TalonSpec.scala
@@ -11,5 +11,19 @@ class TalonSpec extends WordSpec with Matchers {
         assert(!talon.equals(talon.shuffle()))
       }
     }
+    "droped" should {
+      val (droped, card) = talon.drop()
+      "have one card less" in {
+        droped.cards.size should be {
+          talon.cards.size -1
+        }
+      }
+      "not contain droped card" in {
+        droped.cards.contains(card) should be (false)
+      }
+      "still have same order" in {
+        droped.cards should be (talon.cards.tail)
+      }
+    }
   }
 }
