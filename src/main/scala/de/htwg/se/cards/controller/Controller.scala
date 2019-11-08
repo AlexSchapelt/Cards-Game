6598d45e2ed69a950adaa5db63f7cae93114eb53
Alex Schapelt <al-schapelt@t-online.de>
2019-11-08 13:01:13 +0100

    fixed Tests

--- src/main/scala/de/htwg/se/cards/controller/Controller.scala
+++ src/main/scala/de/htwg/se/cards/controller/Controller.scala
@@ -1,8 +1,7 @@
 package de.htwg.se.cards.controller
 
-import de.htwg.se.cards.model.{Player, Talon}
 import de.htwg.se.cards.util.Observable
 
-class Controller() extends Observable{
+class Controller() extends Observable {
 
 }
--- src/main/scala/de/htwg/se/cards/model/Status.scala
+++ src/main/scala/de/htwg/se/cards/model/Status.scala
@@ -1,19 +1,33 @@
 package de.htwg.se.cards.model
 
-import scala.collection.mutable
 import scala.io.Source
 
-case class Status(talon: Talon, queue: mutable.Queue[Player]) {
+case class Status(talon: Talon, queue: List[Player]) {
   private val file = Source.fromFile("./statusCover.txt")
   private val cover = file.getLines.mkString
   file.close()
 
+  def draw(): Status={
+
+    for(p <- queue) {
+
+    }
+    this.copy()
+  }
+
+  def shuffle(): Status = {
+    this.copy(talon = talon.shuffle())
+  }
+
   def nextPlayer(): Status = {
-    queue.enqueue(queue.dequeue())
-    this
+    this.copy(queue = queue.tail :+ queue.head)
+  }
+
+  def current():Player = {
+    queue.head
   }
 
   override def toString: String = {
-    String.format(cover, queue.front)
+    String.format(cover, queue.head)
   }
 }
