a43f01a6ab120e082174485db55014b2bcdf11d1
Alex Schapelt <al-schapelt@t-online.de>
2019-12-06 08:35:10 +0100

    changed regex in tui to try monad

--- src/main/scala/de/htwg/se/cards/aview/Tui.scala
+++ src/main/scala/de/htwg/se/cards/aview/Tui.scala
@@ -3,11 +3,14 @@ package de.htwg.se.cards.aview
 import de.htwg.se.cards.controller.Controller
 import de.htwg.se.cards.util.Observer
 
+import scala.util.Try
+
 class Tui(controller: Controller) extends Observer {
   controller.add(this)
 
   def processInputLine(input: String): Unit = {
-    val toPlay = "([0-9]+ )+".r
+    val list: List[String] = input.split(" ").toList
+    val toPlay: Try[List[Int]] = Try(list.map(x => x.toInt))//"([0-9]+ )+".r
     input match {
       //case "" => controller.addPlayer
       case "s" => controller.shuffle()
@@ -15,8 +18,7 @@ class Tui(controller: Controller) extends Observer {
       case "d" => controller.draw()
       case "z" => controller.undo()
       case "y" => controller.redo()
-      case toPlay(x) => controller.play(input.split(" ").toList.map(x => x.toInt))
-      case _ =>
+      case _ => if(toPlay.isSuccess) controller.play(toPlay.get)
     }
   }
 
