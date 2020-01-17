69a620972522e82a08f9d813552b431a8c75dd8c
alexSchapelt <al-schapelt@t-online.de>
2020-01-17 15:19:56 +0100

    tried to get 100% coverage in FileIO

--- src/test/scala/de/htwg/se/cards/model/fileIOComponent/xmlImpl/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/cards/model/fileIOComponent/xmlImpl/FileIOSpec.scala
@@ -6,15 +6,15 @@ import de.htwg.se.cards.model.talonComponent.talonImpl.Talon
 import de.htwg.se.cards.util.{DeckSingleton, MauRuleStrategy}
 import org.scalatest.{Matchers, WordSpec}
 
-class FileIOSpec extends WordSpec with Matchers{
+class FileIOSpec extends WordSpec with Matchers {
   val fileIO = new FileIO
-  val status = StatusFacade(Talon(DeckSingleton.cards), List(Player("p1", Nil)), Nil, new MauRuleStrategy)
+  val status = StatusFacade(Talon(DeckSingleton.cards), List(Player("p1", List(DeckSingleton.cards.head))), Nil, new MauRuleStrategy)
   "a FileIO after save and load" should {
     "restore the old status" in {
       fileIO.save(status)
       val n = status.init
       n.current.cards.size should not be status.current.cards.size
-      fileIO.load.current.cards.size should be (status.current.cards.size)
+      fileIO.load.current.cards.size should be(status.current.cards.size)
     }
   }
 }
